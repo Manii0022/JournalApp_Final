c178ab7d9685dcc90a7fd986c275c58c46781f80
Manish <manishlodhi0224@gmail.com>
2025-11-10 18:28:09 +0530

    code formatting

--- src/main/java/net/engineeringdigest/JournalApp/repository/UserRepositoryImpl.java
+++ src/main/java/net/engineeringdigest/JournalApp/repository/UserRepositoryImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-
 import java.util.List;
 
 public class UserRepositoryImpl {
