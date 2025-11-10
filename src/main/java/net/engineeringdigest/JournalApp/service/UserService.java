745128b0b7d011ff9dcca5fa2ce33666e77921d4
Manish <manishlodhi0224@gmail.com>
2025-11-10 18:28:16 +0530

    code formatting

--- src/main/java/net/engineeringdigest/JournalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/JournalApp/service/UserService.java
@@ -3,12 +3,10 @@ package net.engineeringdigest.JournalApp.service;
 import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.JournalApp.entity.User;
 import net.engineeringdigest.JournalApp.repository.UserRepository;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
