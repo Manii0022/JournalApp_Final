2a9823729fdcdfbbd2cff4889b17f0cfa803dfce
Manish <manishlodhi0224@gmail.com>
2025-11-10 18:27:54 +0530

    code formatting

--- src/main/java/net/engineeringdigest/JournalApp/entity/User.java
+++ src/main/java/net/engineeringdigest/JournalApp/entity/User.java
@@ -1,12 +1,10 @@
 package net.engineeringdigest.JournalApp.entity;
 
 import lombok.*;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +27,7 @@ public class User {
     private String password;
 
     private String email;
-    private boolean sentimentAnalysis;
+    private boolean getUpdates;   // get automatic updates via email
 
     @DBRef            // like a foreign key used to reference another document
     private List<JournalEntry> journalEntries=new ArrayList<>();
